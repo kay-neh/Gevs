605401ce8eb9681d211951b50e122077248db4fd
kayneh <ifeanyimbanefo@gmail.com>
2024-01-01 19:55:56 +0100

    chore: updated bottom nav items

--- app/src/main/java/com/example/gevs/ui/user/admin/AdminMainActivity.java
+++ app/src/main/java/com/example/gevs/ui/user/admin/AdminMainActivity.java
@@ -2,18 +2,22 @@ package com.example.gevs.ui.user.admin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.gevs.R;
 import com.example.gevs.databinding.ActivityAdminMainBinding;
 import com.example.gevs.ui.auth.loginvoter.VoterLoginActivity;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -32,7 +36,7 @@ public class AdminMainActivity extends AppCompatActivity {
         assert navHostFragment != null;
         NavController controller = navHostFragment.getNavController();
 
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.adminDashboardFragment, R.id.adminVotersFragment, R.id.adminCandidatesFragment, R.id.adminSettingsFragment).build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.adminDashboardFragment, R.id.adminVotersFragment, R.id.adminCandidatesFragment, R.id.adminResultFragment).build();
         //Toolbar
         NavigationUI.setupWithNavController(binding.adminHomepageToolbar, controller, appBarConfiguration);
         //Bottom NavBar
@@ -52,6 +56,24 @@ public class AdminMainActivity extends AppCompatActivity {
             }
         };
 
+        binding.adminHomepageToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.home_about) {
+                    showAboutDialog();
+                }
+                if (id == R.id.home_dark_mode) {
+
+                }
+                if (id == R.id.home_log_out) {
+                    showLogOutDialog();
+                }
+                return false;
+            }
+        });
+
+
     }
 
     @Override
@@ -64,6 +86,38 @@ public class AdminMainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mAuth.removeAuthStateListener(authStateListener);
+    }
+
+    public void showLogOutDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("Log out?");
+        builder.setMessage("You are about to sign out");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                mAuth.signOut();
+            }
+        });
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                //
+            }
+        });
+        builder.show();
+    }
+
+    public void showAboutDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("About the app");
+        builder.setMessage(R.string.lorem_ipsum);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.show();
     }
 
 
